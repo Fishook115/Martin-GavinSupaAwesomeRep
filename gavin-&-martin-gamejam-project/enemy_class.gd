3cee9c6bd31629f5c430e7666905352167a23f0b
GrinwithGavin <gavin.quisenberry@gmail.com>
2026-07-01 12:34:06 -0500

    small enemy class implementation

--- gavin-&-martin-gamejam-project/enemy_class.gd
+++ gavin-&-martin-gamejam-project/enemy_class.gd
@@ -1,13 +1,17 @@
 class_name Enemy extends CharacterBody3D
 var health : float = 5
 var speed : float = 6
-
-
+var damage : float = 10
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
+	if is_node_ready():
+		_movement_logic()
+	pass
+
+func _movement_logic():
 	pass
